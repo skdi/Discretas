e3f55ed98460aadc06d7670d106a3cc962a335ad
Skdi <jddhghf@gmail.com>
2017-05-26 17:23:37 -0500

    Update cripto_afin.cpp

--- cripto_afin.cpp
+++ cripto_afin.cpp
@@ -1,4 +1,7 @@
 #include "cripto.h"
+#include <iostream>
+
+using namespace std;
 
 cripto::cripto(int clave1){
     setclave(clave1,0);
@@ -28,8 +31,10 @@ int cripto::getclave(){
 
 string cripto::codifica(string mensaje){
     string temp=mensaje;
+    int t;
     for(unsigned int i=0;i<mensaje.length();i++){
-        temp[i]=alfabeto[(a+b)%alfabeto.length()];
+        t=alfabeto.find(mensaje[i]);
+        temp[i]=alfabeto[(t+b)%alfabeto.length()];
     }
     return temp;
 }
@@ -52,55 +57,26 @@ string cripto::decodifica(string mensaje){
     return temp;
 }
 
-void cripto::gcd(int a, int b,int &d,int &sx,int& ty){
-    int r,s,t,r1,r2,s1,s2,t1,t2,q;
-    r1=a;r2=b;
-    s1=1;s2=0;
-    t1=0;t2=1;
-    while(r2>0){
-        q=r1/r2;//inicializamos el valor de q
-
-        r=r1-q*r2;//iniciamos r como el valor "a" -el cociente por "b"
-        r1=r2;r2=r;//actualizamos posiciones r1 ahora es b ,r2 ahora es el modulo a y b
-
-        s=s1-q*s2;//hallamos el modulo de s1 y s2
-        s1=s2;s2=s;//actualizamos su posicion
-
-        t=t1-q*t2;//hallamos el modulo de t1 y t2
-        t1=t2;t2=t;//actualizamos su posicion
-    }
-    sx=s1;//retornamos X
-    ty=t1;//retornamos Y
-    d=r1;
-
-}
-
-int cripto::inverso(int a,int b){
-    int invert,x,y,d;
-    gcd(a,b,d,x,y);
-    if(d>1)
-        return 0;
-    else{
-        if(x<0)
-            invert=b+x;
-        else invert=x;
-    }
-    return invert;
-}
 
 string cripto::decodifica2(string mensaje){
-    int inv_a;
+    int inv_a=0;
     string temp=mensaje;
-    int t;
+    int t=0,t1=0;
+    for(unsigned int i=0;i<alfabeto.length();i++){
+        t=(a*i)%alfabeto.length();
+        if(t==1)
+            inv_a=i;
+    }
     for(unsigned int i=0;i<mensaje.length();i++){
-        t=alfabeto.find(mensaje[i]);
-        inv_a=inverso(alfabeto.length(),t);
-        temp[i]=alfabeto[((a-b)*inv_a)%alfabeto.length()];
+        t1=alfabeto.find(mensaje[i]);
+        temp[i]=alfabeto[(inv_a*(t1-b))%alfabeto.length()];
     }
     return temp;
 
+
 }
-////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
 #ifndef CRIPTO_H
 #define CRIPTO_H
 
@@ -125,8 +101,6 @@ class cripto{
     string codifica2(string mensaje);
     string decodifica(string mensaje);
     string decodifica2(string mensaje);
-    int inverso(int n,int a);
-    void gcd(int a1, int m, int &d, int &sx, int &ty);
     void setclave(int clave1, int b);
     int getclave();
 };
@@ -134,21 +108,19 @@ class cripto{
 
 
 #endif // CRIPTO_H
-
-
+///////////////////////////////////////////////////////////////////////////////////////
 #include "cripto.h"
 #include <iostream>
 using namespace std;
 
 int main(){
 
-  cripto *emisor=new cripto(3,2);
+  cripto *emisor=new cripto(2,1);
   string mensaje="hola como va todo";
   cout<<mensaje<<endl;
   string mensajecifrado=emisor->codifica2(mensaje);
   cout<<mensajecifrado<<endl;
-  cripto *receptor=new cripto(3,2);
-  string mensajeplano=receptor->decodifica2(mensajecifrado);
+  string mensajeplano=emisor->decodifica2(mensajecifrado);
   cout<<mensajeplano<<endl;
   return 0;
 
