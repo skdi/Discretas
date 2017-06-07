2a0d666b3ea63667ab1e44f8e7622c8fc2efeae2
Skdi <jddhghf@gmail.com>
2017-06-07 11:03:02 -0500

    Update expomod.cpp

--- expomod.cpp
+++ expomod.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 
 
-int expomodular(int base,int exp,int mod){
+
+int expomod(int base,int exp,int mod){
     int result=1,expmod;
 
     if(exp%2!=0)
-        result=base;
+        result=base%mod;
     exp=exp/2;
 
     while(exp>0){
@@ -22,10 +23,40 @@ int expomodular(int base,int exp,int mod){
 
 
 
+int expmod(int base, int exp,int mod) {
+    int bina=0;
+    int resul=1;
+    int d=base%mod;
+    cout<<"exp"<<"\t"<<"expmod2"<<"\t"<<"base2"<<"\t"<<"resultado"<<"\t"<<"exp/2"<<endl;
+    cout<<exp<<"\t"<<bina<<"\t"<<d<<"\t"<<resul<<"\t"<<"\t"<<exp/2<<endl;
+    bina=exp%2;
+    if(bina==1){
+        resul=d;
+    }
+    exp=exp/2;
+
+    while (exp>0){
+        //cout<<exp<<"\t"<<bina<<"\t"<<d<<"\t"<<resul<<"\t"<<exp/2<<endl;
+        bina= exp%2;
+        d=(d*d)%mod;
+        if(bina!=0){
+            resul=(d*resul)%mod;
+        }
+        exp=exp/2;
+        cout<<"   "<<"\t"<<bina<<"\t"<<d<<"\t"<<resul<<"\t"<<"\t"<<exp<<endl;
+    }
+    return resul;
+
+}
+
+
+
 int main() {
 
 
-    cout<<"expm : "<<expomodular(7,644,645)<<endl;
+    cout<<"expm : "<<expomod(667,1271,2537)<<endl;
+    cout<<"expm : "<<expomod(1947,1271,2537)<<endl;
+    cout<<"expm : "<<expomod(671,1271,2537)<<endl;
 
     return 0;
 }
