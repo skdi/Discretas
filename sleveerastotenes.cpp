37bb669dbb9e013e96558d1cbf6abd807a7d1ce0
Skdi <jddhghf@gmail.com>
2017-06-28 19:45:36 -0500

    Update sleveerastotenes.cpp

--- sleveerastotenes.cpp
+++ sleveerastotenes.cpp
@@ -1,32 +1,41 @@
+#include <ctime>
 #include <iostream>
-#define ll long long
+#define ll unsigned long long
+unsigned t0, t1;
 
 using namespace std;
-void sleve(ll n){
+ll n=1000000000;
+bool * isPrime = new bool [n/2]();
 
-    bool * isPrime = new bool [n/2]();
+void sleve(){
 
-    for (ll i=0; i < n/2; ++i)
+    for (ll i=0; i < n/2; ++i)//inicializacion de la mitad del vector en 1
         isPrime [i] = true;
 
-    cout<< 2 << "\n";
-    cout<< 3 << "\n";
 
-    for (ll i = 5, t = 2; i < n; i += t, t = 6 - t) {
+    for (ll i = 5, t = 2; i < n; i += t, t = 6 - t) {//inicio en 5 , salto de 2 o 4 ,recorrido total ,saltamos de t en t
 
-        if (isPrime [i / 3]) {
-
-            cout<< i << "\n";
-
-            for (ll j = i * i, v = t; j < n; j += v * i, v = 6 - v)
-                isPrime [j / 3] = false;
+        if (isPrime [i / 3]) {//si es primo
+            for (ll j = i * i, v = t; j < n; j += v * i, v = 6 - v)//saltamos en i^2 ,v vale 2 o 4 ,recorrido total,aumento de 2 o 4por las posicion actual 
+                isPrime [j / 3] = false;//cribo el resultado
         }
     }
+}
+void print(){
+    for(ll i = 5, t = 2; i < n; i += t, t = 6 - t) 
+        if (isPrime [i / 3]) 
+            cout<<i<<'\n';
 }
 
 int main()
 {
-    sleve(5024);
-    cout << "Hello World!" << endl;
+    cout<< 2 << "\n";
+    cout<< 3 << "\n";
+    t0=clock();
+    sleve();
+    t1 = clock();
+    double time = (double(t1-t0)/CLOCKS_PER_SEC);
+    //cout << "Execution Time: " << time << endl;
+    print();
     return 0;
 }
